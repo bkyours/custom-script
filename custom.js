7aa761905490aec8d083e3d6d66cc6523a939b02
Bibek Sharma Chapagain <bibeksharma@lftechnology.com>
2019-03-07 22:53:07 +1100

    Update custom.js

--- custom.js
+++ custom.js
@@ -149,6 +149,7 @@ $(document).ready(function () {
             '<div class="col-xs-12"><a class="icon-with-text-container" href="https://www.twitter.com/barkyours"><i class="fa fa-twitter icon-part"></i> <div class="text-part">Twitter</div></a></div>' +
             '</div></div></div>' +
             '<div class="row footer-link text-center" style="font-size: 14px;color: #959494;">All rights reserved ©2018 BarkYours</div></div></footer>');
+            $('.footer-links a').attr("target", '_blank')
     }
 
     function initializeFAQPopUp() {
