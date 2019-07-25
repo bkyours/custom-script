0484acd46728929f9b2e6e760d705c3bb135825c
Bibek Sharma Chapagain <bipashant@gmail.com>
2019-07-25 10:56:56 +1000

    Update landing_page.js

--- landing_page.js
+++ landing_page.js
@@ -21,7 +21,9 @@ $(document).ready(function(){
               "<div><p style='color: #000; font-size: 16px; font-weight: 400; padding: 0 30px;'> " + topSectionContent3 + "<span><a href='#whatIsBarkYoursAllAbout' <i class='fa fa-play' style='margin-left: 10px; color: #0765a8; font-size: 22px;'></i></a></span></p></div>" +
           "</div>" +
 
-          "</div>";
+          "</div>" +
+              
+              '<div class="full-width text-center" style="margin-top: 20px"> <a class="view-all-listing-link-btn" href="https://www.barkyours.com/?view=grid"> VIEW ALL LISTINGS</a></div>';
 
       var whatIsBarkYourSection =  "<div class='what-is-bark-about-section' id='whatIsBarkYoursAllAbout' style='margin-top: 4.5em;background: #f0f0f0; overflow: hidden'><div class='col-12 text-center' style='padding: 30px;'><h1><b>" + whatIsBarkYourSectionTitle1 + "</b></h1>" +
           "<p style='color: #000; font-size: 16px; font-weight: 400;'>" + whatIsBarkYourSectionContent1 + "</p>" +
