c3e6fcb5e2c159dd8f107b6736af7705045b9c9e
Bibek Sharma Chapagain <bipashant@gmail.com>
2019-09-23 22:18:18 +1000

    Update custom.js

--- custom.js
+++ custom.js
@@ -135,9 +135,9 @@ $(document).ready(function(){
     var mappedSizeArray = ['XS:', 'S:', 'M:', 'L:', 'XL:', '2XL:', '3XL:', '2T:', '3T:', '4T:', '5T:'];
 
     $(".listing-details-container b").each(function () {
-        $(this).parent().parent().hide();
         var size = $(this).html();
         if (mappedSizeArray.indexOf(size) >= 0 ) {
+            $(this).parent().parent().hide();
             listingSizeUrl[sizeArray[mappedSizeArray.indexOf(size)]] = $(this).siblings()[0].getAttribute("href");
         }
     });
