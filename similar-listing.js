8497fb10a9bad51fa4d41e7d49a5a487f051956b
Bibek Sharma Chapagain <bipashant@gmail.com>
2019-10-16 12:01:31 +1100

    Update similar-listing.js

--- similar-listing.js
+++ similar-listing.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     var sizeArrayIdInDom = [];
     var sizeUrlArrayIdInDom = [];
 
-   function sizeFieldPresent(){
+    function sizeFieldPresent(){
         // Checking if Size checkboxes are present in DOM
         var visibility = false;
         $.each(sizeArrayIdInDom, function( index, value ){
@@ -62,7 +62,7 @@ $(document).ready(function(){
 
     function showHideUrlFieldOnDefault(){
 
-        
+
         $.each(sizeUrlArrayIdInDom, function( index, domID ){
             $("#" + domID).hide();
         });
@@ -102,7 +102,6 @@ $(document).ready(function(){
 
     function showSizeURLFieldsTimer(){
         if($("#listing_title").length > 0){
-
             $("input[name='custom_fields[76086][]'").each(function(index, elm){
 
                 var id = $(elm).attr('id');
@@ -111,20 +110,18 @@ $(document).ready(function(){
                 var labelText = $("label[for='" + id + "'").text()
 
                 var associatedURLFieldLabel = $($("label:textEquals('"+labelText+"')")[1]);
-                var associatedURLFieldInput = $("#" + associatedURLFieldLabel.attr("for"));
+                var associatedURLFieldInput = associatedURLFieldLabel.attr("for");
                 sizeUrlArrayIdInDom.push(associatedURLFieldInput);
 
             })
-            
-            
+
             if(sizeFieldPresent()){
                 showLabel();
-                // hiding all url field at 
+                // hiding all url field at
                 showHideUrlFieldOnDefault();
                 // If size checkbox presents, add a new class size-checkbox
                 $("#" + sizeArrayIdInDom[0]).parents(".checkbox-group-container").find("input").addClass("size-checkbox");
             }
-            debugger;
             $(".size-checkbox").change(function () {
                 handleCheckboxChange($(this));
             });
@@ -141,8 +138,10 @@ $(document).ready(function(){
 // For Show listing page
 $(document).ready(function(){
     listingSizeUrl = {};
-    var sizeArray = ['XS', 'S', 'M', 'L', 'XL', '2XL', '3XL', '2T', '3T', '4T', '5T'];
-    var mappedSizeArray = ['XS:', 'S:', 'M:', 'L:', 'XL:', '2XL:', '3XL:', '2T:', '3T:', '4T:', '5T:'];
+    var mappedSizeArray = [];
+    $.each(sizeArray, function(index, size){
+        mappedSizeArray.push(size + ':');
+    });
 
     $(".listing-details-container b").each(function () {
         var size = $(this).html();
@@ -156,11 +155,12 @@ $(document).ready(function(){
     $("#sizeFilter").change(function(){
         var selectedSize = $(this).val();
         var selectedText = $("#sizeFilter option:selected").text().replace(/(\r\n\t|\n|\r\t)/gm, "");
-        currentUrl = location.href;
-        destinationUrl = listingSizeUrl[selectedSize];
-        if(destinationUrl != undefined && currentUrl !== destinationUrl)
+        var destinationUrl = listingSizeUrl[selectedSize];
+        var currentListingId = $("#listing_id").val();
+        var destinationListingId = destinationUrl.split("https://www.barkyours.com/en/listings/")[1].split("-")[0];
+
+        if(destinationUrl != undefined && currentListingId != destinationListingId)
         {
-            debugger;
             location.href = destinationUrl + "?selectedSize=" + selectedText; // redirect only if the current URL is same
 
         }
