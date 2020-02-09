8bb11a842193d2eb69e7e60407971cd43fa250ea
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-02-09 20:41:04 +1100

    Update checkout-fields.js

--- checkout-fields.js
+++ checkout-fields.js
@@ -1,110 +1,223 @@
 $(document).ready(function(){
-    if ($("#new_message_form").length) {
-        // New transaction page
-        var listingId = window.location.href.match("listings/(.*)/initiate")[1];
-        var content = httpGet("https://www.barkyours.com/en/listings/"+ listingId);
-        var selectedCharities = [];
-        var allCharities = Object.keys(charities);
-        var charitiesToDisplay = []
-        $(content).find(".listing-details-container .checkbox-group").each(function () {
-            if ($(this).parent().find("b").text() == "Charities (Optional):") {
-                $(this).find(".checkbox-option.selected").each(function () {
-                    selectedCharities.push($(this).find("span").last().text().replace(/(\r\n\t|\n|\r\t)/gm, ""));
-                });
-            }
-        });
+    addCheckOutFilters();
 
-        if(selectedCharities.length > 0)
-            charitiesToDisplay = selectedCharities;
-        else
-            charitiesToDisplay = allCharities;
-        charitiesToDisplay.push("Let BarkYours decide.")
+    function addCheckOutFilters() {
+        if ($(".listing-details-container").length) {
 
-        if(charitiesToDisplay.length > 0){
-            var charitiesRadioButton = '<h3>Select a Charity</h3><div class="info-text-container"><div class="info-text-icon"></div> <div class="info-text-content1"> <p>BarkYours is proud to donate a portion of every sale to charity. Please select your preferred charity.</p> </div> </div>';
-            var count = 0;
-            $(charitiesToDisplay).each(function () {
-                var charityLink = charities[this];
-                var charityId = "charity-" + count;
-                charitiesRadioButton += '<input type="radio" class="charity-radio-btn" name="charity"  id="'  + charityId + '" value="'+ this +'"> <label class="inline" style="font-weight: 100;" for="' + charityId + '">' + this + '</label> <br>';
-                count += 1;
-            });
-            $("#transaction-form .preauthorize-section").prepend(charitiesRadioButton + '<br>');
+            var sizeFilterOptions = [];
+            var colorFilterOptions = [];
+            var charityFilterOptions = [];
 
-            $("#message").attr("name", "message_temp").attr("id", "message_temp");
-
-            $('<textarea name="message" id="message" class="text_area hide" style="overflow: hidden; overflow-wrap: break-word; resize: horizontal; height: 120px;"></textarea>').insertAfter("#message_temp");
-
-            $('<input name="charity-text"  class="hide" id="charityText">').insertAfter("#message_temp");
-
-            $("#message").val($("#message_temp").val());
-
-            $("#letBarkyoursDecide").prop("checked", true);
-
-            $("#message_temp").on('change keyup paste', function () {
-                updateMessageBox();
-            });
-
-            $(".charity-radio-btn").on('change', function () {
-                $("#charityText").val($(this).val());
-                updateMessageBox();
-            })
+            var filtersContainerStr = "<div class='filter-dropdowns'>";
 
 
-            function updateMessageBox(){
-                var text = '';
-                if($("#charityText").val().length){
-                    text += "Charity: " + $("#charityText").val() + "\n";
+            $(".listing-details-container .checkbox-group").each(function () {
+                if ($(this).parent().find("b").text() == "Size:") {
+                    if ($("aside form").length > 0) {
+                        $(this).parent().parent().hide();
+                        filtersContainerStr += "<div class='row' id='sizeFilterWrapper'></div>";
+                    }
+                    $(this).find(".checkbox-option.selected").each(function () {
+                        sizeFilterOptions.push($(this).find("span").last().text().replace(/(\r\n\t|\n|\r\t)/gm, ""));
+                    });
+
                 }
-                text += $("#message_temp").val();
-                $("#message").val(text);
-            }
-
-            setTimeout(function(){
-                $("#letBarkyoursDecide").prop("checked", true).trigger('change');;
-            }, 2000);
-
-        }
-    };
-
-    function httpGet(theUrl)
-    {
-        var xmlHttp = null;
-
-        xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false );
-        xmlHttp.send( null );
-        return xmlHttp.responseText;
-    }
-
-
-    if($(".new-listing-form").length){
-        // Listing new/edit page
-
-        var formLoadingTimer = setInterval(checkIfFormLoaded, 500);
-
-        function checkIfFormLoaded(){
-            if($("#listing_title").length > 0){
-                stopInterval();
-
-                if($("[name='custom_fields[121088][]']").length){
-                    // checking if there is charity checkboxes are present
-                    $("label[for='custom_fields_121088']").text("Charities (Optional)");
-                    $('<div class="info-text-container"> <div class="info-text-icon"> <i class="ss-info"></i> </div> <div class="info-text-content"> <p>BarkYours is proud to donate a portion BarkYours\' proceeds from every sale to charity.  Buyers are given the choice of which charity to donate to.  Select the charities you want your buyers to choose from here, or skip this step to allow them to pick any charity on the list.  If you would like to see your preferred charity added, email <a href="mailto:info@barkyours.com">info@barkyours.com</a>.</p> </div></div>').insertAfter("label[for='custom_fields_121088']")
-
-                    $.each($("[name='custom_fields[121088][]']"), function(index, item){
-                        label = $("label[for='"+$(item).attr('id')+"']");
-                        url = charities[label.text()];
-                        label.html('<a href= "'+ url+'"target="_blank" >'+$(label).text()+'</a>');
+                else if ($(this).parent().find("b").text() == "Colors:") {
+                    if ($("aside form").length > 0) {
+                        $(this).parent().parent().hide();
+                        filtersContainerStr += "<div class='row' id='colorFilterWrapper'></div>";
+                    }
+                    $(this).find(".checkbox-option.selected").each(function () {
+                        colorFilterOptions.push($(this).find("span").last().text().replace(/(\r\n\t|\n|\r\t)/gm, ""));
                     });
                 }
+                else if ($(this).parent().find("b").text() == "Charities:") {
+                    if ($("aside form").length > 0) {
+                        $(this).parent().parent().hide();
+                        filtersContainerStr += "<div class='row' id='charityFilterWrapper'></div>";
+                    }
+                    // Commenting these lines because we are going to show charity dropdown on next page.
+//                     $(this).find(".checkbox-option.selected").each(function () {
+//                         charityFilterOptions.push($(this).find("span").last().text().replace(/(\r\n\t|\n|\r\t)/gm, ""));
+//                     });
+                }
+            });
+
+            filtersContainerStr += "</div>";
+
+            $(filtersContainerStr).insertBefore(".enabled-book-button");
+
+            if (sizeFilterOptions.length || colorFilterOptions.length || charityFilterOptions.length) {
+
+                sizePresent = false;
+                colorPresent = false;
+                charityPresent = false;
+
+                if (sizeFilterOptions.length) {
+                    var s = $('<select id="sizeFilter"/>');
+
+                    $('<option />', {value: null, text: "Size"}).appendTo(s);
+
+                    $.each(sizeFilterOptions, function (index, value) {
+                        $('<option />', {value: value, text: value}).appendTo(s);
+                    });
+
+                    s.appendTo($('.filter-dropdowns #sizeFilterWrapper'));
+
+                    // Select the size by default on similar listing with different price
+                    var selectedSize = getUrlParameter("selectedSize");
+
+                    $("#sizeFilter option").filter(function (index) {
+                        return $(this).text() === selectedSize;
+                    }).attr('selected', 'selected');
+
+                    $("<span class='buyer-size-price-change-info'>Prices may vary with size selection.</span>").prependTo($('.filter-dropdowns #sizeFilterWrapper'));
+                    sizePresent = true;
+                }
+
+                if (colorFilterOptions.length) {
+                    var s = $('<select id="colorFilter"/>');
+
+                    $('<option />', {value: null, text: "Color"}).appendTo(s);
+
+                    $.each(colorFilterOptions, function (index, value) {
+                        $('<option />', {value: value, text: value}).appendTo(s);
+                    });
+
+                    s.appendTo($('.filter-dropdowns #colorFilterWrapper'));
+
+                    colorPresent = true;
+
+                }
+
+                if (charityFilterOptions.length) {
+                    var s = $('<select id="charityFilter"/>');
+
+                    $('<option />', {value: null, text: "Charity"}).appendTo(s);
+
+                    $.each(charityFilterOptions, function (index, value) {
+                        $('<option />', {value: value, text: value}).appendTo(s);
+                    });
+
+                    s.appendTo($('.filter-dropdowns #charityFilterWrapper'));
+
+                    charityPresent = true;
+                }
+
+                $(".filter-dropdowns").append("<a class='listing-author-contact-button' id='listingBuyButton' style = 'background-color: #0765a8; color: #fff;' href='#'> <div class='content'>Proceed to Checkout</div></a>")
+
+                $(".enabled-book-button").hide();
+
+                $("#listingBuyButton").click(function (e) {
+                    e.preventDefault();
+                    var listingID = $("#listing_id").val();
+
+                    var size = $("#sizeFilter").val();
+                    size = size == "Size" ? null : size;
+
+                    var color = $("#colorFilter").val();
+                    color = color == "Color" ? null : color;
+
+                    var charity = $("#charityFilter").val();
+                    charity = charity == "Charity" ? null : charity;
+
+
+
+                    var shippingEnabled = $("#delivery_shipping").prop('checked');
+                    var pickupEnabled = $("#delivery_pickup").prop('checked');
+
+                    url = "https://www.barkyours.com/en/listings/" + listingID + "/initiate?";
+
+                    readyToRedirect = true;
+
+                    if (size && size.length) {
+                        url += "size=" + size + "&";
+                    }
+
+                    if (color && color.length) {
+                        url += "color=" + color + "&";
+                    }
+
+                    if (charity && charity.length) {
+                        url += "charity=" + charity + "&";
+                    }
+
+                    if (shippingEnabled) {
+                        url += "delivery=shipping&";
+                    }
+
+                    if (pickupEnabled) {
+                        url += "delivery=pickup&";
+                    }
+                    toastr.clear();
+
+                    if (sizePresent && size == null) {
+                        readyToRedirect = false;
+                        toastr.error("You missed the Size :)")
+                    }
+
+                    if (colorPresent && color == null) {
+                        readyToRedirect = false;
+                        toastr.error("You missed the Color :)")
+                    }
+
+                    if (readyToRedirect) {
+                        var loggedIn = $(".AvatarDropdown").length > 0;
+
+                        if (!loggedIn) {
+                            var listingTitle = $("#listing-title").text().replace(/(\r\n\t|\n|\r\t)/gm, "");
+                            var listingPrice = $(".listing-price-amount").text().replace(/(\r\n\t|\n|\r\t)/gm, "");
+                            var listingUrl = window.location.href;
+                            var listingThumb = null;
+
+                            if ($("#listing-image-link img").length) {
+                                listingThumb = $("#listing-image-link img")[0].src.replace("/big/", "/thumb/");
+                            }
+                            var shippingLbl = "";
+                            if ($(".shipping-options-label").length) {
+                                shippingLbl = $(".shipping-options-label").text().replace(/(\r\n\t|\n|\r\t)/gm, "");
+                            }
+
+                            localStorage.setItem("lastUpdate", new Date().getTime());
+                            localStorage.setItem("listingTitle", listingTitle);
+                            localStorage.setItem("listingURL", listingUrl);
+                            localStorage.setItem("listingThumb", listingThumb);
+                            localStorage.setItem("listingPrice", listingPrice);
+                            localStorage.setItem("shippingLbl", shippingLbl);
+
+                            window.location.href = "https://www.barkyours.com/en/login?checkout=true";
+                        }
+                        else {
+                            window.location.href = url;
+                        }
+                    }
+                });
 
             }
         }
 
-        function stopInterval() {
-            clearInterval(formLoadingTimer);
-        }
+        if ($("#new_message_form").length) {
 
+
+            var size = getUrlParameter("size");
+            var color = getUrlParameter("color");
+            var charity = getUrlParameter("charity");
+            var text = "";
+
+            if (size && size.length && size !== "Size") {
+                text = "Size: " + size + "\n";
+            }
+
+            if (color && color.length && color !== "Color") {
+                text += "Color: " + color + "\n";
+            }
+
+            if (charity && charity.length && charity !== "Charity") {
+                text += "Charity: " + charity + "\n\n";
+            }
+
+            $("#message").focus().val(text);
+
+        }
     }
 });
