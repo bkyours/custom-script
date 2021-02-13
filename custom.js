e1134638c97aee5220e5d5f6da226638d8a9cd81
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2021-02-14 01:18:12 +1100

    Refactoring: Removed the Landing page dependencies

--- custom.js
+++ custom.js
@@ -1,9 +1,6 @@
 infiniteScrollToPagination();
 
 $(document).ready(function () {
-    addFooter();
-    initializeFAQPopUp();
-    displayLandingPageOrHomepage();
     addDecimalToListingPagePrice();
     showHiddenNotification();
     initializeLoginInfo();
@@ -20,80 +17,11 @@ $(document).ready(function () {
        }
     }
 
-    function displayLandingPageOrHomepage() {
-        var currentURL = window.location.href;
-        var showListingList = currentURL.indexOf("category") >= 0 || currentURL.indexOf("filter_option") >= 0 || currentURL.indexOf("view=list") >= 0 || currentURL.indexOf("view=map") >= 0 || currentURL.indexOf("view=grid") >= 0 || currentURL.indexOf("price_min") >= 0 || currentURL.indexOf("?q=") >= 0 || currentURL.indexOf("?page=") >= 0 || currentURL.indexOf("price_max") >= 0;
-
-        if (!showListingList) {
-            addBannerForNotLoggedIn();
-            addViewAllListingLink();
-            displayLandingPage();
-
-        } else {
-            setInterval(customizeGrid, 1000);
-        }
-    }
     
     if($("title").text() == "Bark yours"){
       $("title").text("BarkYours - Gifts for People Who Love Their Dogs");
     }
 
-    function addBannerForNotLoggedIn() {
-
-        if ($("#homepage-filters").length > 0) {
-            console.log("homepage detected");
-           
-            if ($(".marketplace-lander-content-title").length > 0) {
-            }
-            else {
-                console.log("user is logged In");
-                $(".marketplace-lander").append('<div class="coverimage"><figure class="marketplace-cover fluidratio"> <div class="lander-content marketplace-lander-content"> <h1 class="marketplace-lander-content-title">' + marketplace_slogan + '</h1> <p class="marketplace-lander-content-description">' + marketplace_description + '</p> </div> </figure> </div>');
-            }
-        } else {
-            console.log("Not Homepage");
-            $(".title-container").css("background", "#fff").css("border-bottom", "1px solid rgba(0,0,50,0.1)");
-        }
-    }
-
-    function addViewAllListingLink() {
-
-        if ($(".home-fluid-thumbnail-grid").length > 0) {
-            $("<div class='full-width text-center' style='margin-top: 20px'> <a class='view-all-listing-link-btn' href='https://www.barkyours.com/?view=grid'> VIEW ALL LISTINGS</a></div>").insertAfter($(".home-fluid-thumbnail-grid"));
-        }
-    }
-
-
-    function addViewFieldOnSearch() {
-        var view = $("#view").val();
-        $("#topbar-container form").prepend('<input type="hidden" name="view" id="view1" value="' + view + '">');
-    }
-
-    function customizeGrid() {
-        $(".home-fluid-thumbnail-grid").css("visibility", "visible");
-        $(".home-fluid-thumbnail-grid-item").each(function (index) {
-            if (!$(this).hasClass("customized")) {
-                $(".home-fluid-thumbnail-grid-author-avatar").remove();
-                var priceTag = $(this).find(".fluid-thumbnail-grid-image-price-container");
-                var priceTagSpan = priceTag.find("span");
-                var price = priceTagSpan.text().replace(/(\r\n\t|\n|\r\t)/gm, "");
-                if (price.indexOf('.') == -1) {
-                    priceTagSpan.text(price + ".00");
-                }
-                var title = $(this).find(".fluid-thumbnail-grid-image-title");
-                var authorContainer = $(this).find(".home-fluid-thumbnail-grid-author");
-                var authorLink = $(this).find(".home-fluid-thumbnail-grid-author-name");
-                authorContainer.prepend('<div class="price-container"></div>');
-                authorContainer.prepend(title);
-                authorContainer.append('<div class="info-container"></div>');
-                authorContainer.find(".price-container").prepend(priceTag);
-                authorContainer.find(".info-container").prepend(authorLink);
-                // authorContainer.find(".info-container").prepend(title);
-                var availableWidth = authorContainer.width() - $(this).find(".price-container").width();
-                $(this).find(".info-container").css("width", (availableWidth - 3) + "px");
-                $(this).addClass("customized");
-            }
-        });
-    }
 
     if ($("#profile-listings-list").length) {
         setInterval(customizePeopleGrid, 1000);
@@ -129,141 +57,6 @@ $(document).ready(function () {
                 $(this).find(".info-container").css("width", (availableWidth - 3) + "px");
                 $(this).addClass("customized");
             }
-        });
-    }
-
-    function addFooter() {
-        categoriesLink = '';
-        $.map(categories, function (value, index) {
-            categoriesLink += '<a href="' + value.url + '">' + value.title + '</a></div><div class="col-sm-12 col-xs-6">';
-        });
-        $('body').append('<footer><div class=layout-centered-content><div class="row footer-links"><div class="col-xs-12 col-sm-4">' +
-            '<div class=row><h3>Shop</h3><div class="col-sm-12 col-xs-6">' +
-            categoriesLink +
-            '</div></div></div><div class="col-xs-12 col-sm-4">' +
-            '<div class=row><h3>Learn more</h3><div class="col-sm-12 col-xs-6">' +
-            '<a href=/en/infos/about>About BarkYours</a></div><div class="col-sm-12 col-xs-6">' +
-            '<a class= "faq-popup-trigger" href=#faq-popup>FAQs</a></div><div class="col-sm-12 col-xs-6">' +
-            '<a href=/en/infos/how_to_use>How It Works</a></div><div class="col-sm-12 col-xs-6">' +
-            '<a href=/user_feedbacks/new>Contact Us</a></div><div class="col-sm-12 col-xs-6">' +
-            '<a href=/invitations/new>Invite Others</a></div><div class="col-sm-12 col-xs-6">' +
-            '<a href=/en/infos/privacy>Privacy Policy</a></div><div class="col-sm-12 col-xs-6">' +
-            '<a href=/en/infos/terms>Terms of Use</a></div></div></div>' +
-            '<div class="col-xs-12 col-sm-4">' +
-            '<div class="row"><h3>Follow BarkYours</h3>' +
-            '<div class="col-xs-12 "><a class="icon-with-text-container" href="https://www.facebook.com/barkyours"><i class="fa fa-facebook icon-part"></i> <div class="text-part">Facebook</div></a></div>' +
-            '<div class="col-xs-12"><a class="icon-with-text-container" href="https://www.instagram.com/barkyours"><i class="fa fa-instagram icon-part"></i> <div class="text-part">Instagram</div></a></div>' +
-            '<div class="col-xs-12"><a class="icon-with-text-container" href="https://www.twitter.com/barkyours"><i class="fa fa-twitter icon-part"></i> <div class="text-part">Twitter</div></a></div>' +
-            '<div class="col-xs-12"><a class="icon-with-text-container" href="https://www.pinterest.com/barkyours/"><i class="fa fa-pinterest icon-part"></i> <div class="text-part">Pinterest</div></a></div>' +
-            '</div></div></div>' +
-            '<div class="row footer-link text-center" style="font-size: 14px;color: #959494;">All rights reserved ©2018 BarkYours</div></div></footer>');
-            $('.footer-links a').attr("target", '_blank')
-    }
-
-    function initializeFAQPopUp() {
-        $("#topbar-container a").removeAttr("target")
-        var popUpcontent = '<div id="faq-popup" class="faq-overlay"><div class="faq-popup"><div class="popup-header"><h2>Frequently Asked Questions</h2><a class="close faq-popup-close" href="#">&times;</a><div class="faq-searchbar-wrapper"><div class="faq-search-input-wrapper"><input type="search" class="faq-searchbar" placeholder="Search..."><button type="submit" class="SearchBar__searchButton__1Ck2b" style="background-color:transparent;" data-reactid="54"><svg width="17" height="17" viewBox="336 14 17 17" xmlns="http://www.w3.org/2000/svg"><g opacity=".7" fill="none" fill-rule="evenodd" transform="matrix(-1 0 0 1 352 15)" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M11 11l3.494 3.494"></path><circle cx="6" cy="6" r="6"></circle></g></svg></button></div></div></div><div class="main-content"><div class="box" id = "qns-box"><ul class="question-list">';
-        var qnsListForShopper = $.map(questionAnswerForShopper, function (value, index) {
-            return ('<div class="qns-list"><li id="link-for-shopper-question-' + index + '"><a href=#question-shopper-' + index + ' class="question-link">' + value.qns + '</a></li></div>');
-        });
-        var qnsListForSeller = $.map(questionAnswerForSeller, function (value, index) {
-            return ('<div class="qns-list"> <li id="link-for-seller-question-' + index + '"><a href=#question-seller-' + index + ' class="question-link">' + value.qns + '</a></li></div>');
-        });
-        var qnsListString = '<li id="qnsHeadForShopper"><a href="#qnsAnsHeadForShopper">I am a BarkYours Shopper</a></li>';
-        $.each(qnsListForShopper, function () {
-            qnsListString += this;
-        });
-        qnsListString += '<li id="qnsHeadForSeller"><a href="#qnsAnsHeadForSeller">I am a BarkYours Seller</a></li>';
-        $.each(qnsListForSeller, function () {
-            qnsListString += this;
-        });
-        var popUpcontent1 = '</ul></div><div class="content1 padding-25">';
-        var qnsAnsListForSeller = $.map(questionAnswerForSeller, function (value, index) {
-            return ('<div class="box" id="question-seller-' + index + '">' + '\n<div class="question">' + value.qns + '</div>\n<div class="answer">' + value.ans + '</div>\n<div class="top-link"><span class="fa fa-arrow-up navigate-top">TOP</span>\n</div></div>');
-        });
-        var qnsAnsListForShopper = $.map(questionAnswerForShopper, function (value, index) {
-            return ('<div class="box" id="question-shopper-' + index + '">' + '\n<div class="question" id="">' + value.qns + '</div>\n<div class="answer">' + value.ans + '</div>\n<div class="top-link"><span class="fa fa-arrow-up navigate-top">TOP</span>\n</div></div>');
-        });
-        var qnsAnsListString = '<div class="box faq-category-title" id="qnsAnsHeadForShopper"><span>I am a BarkYours Shopper</span></div>';
-        $.each(qnsAnsListForShopper, function () {
-            qnsAnsListString += this || '';
-        });
-        qnsAnsListString += '<div class="box faq-category-title" id="qnsAnsHeadForSeller"><span>I am a BarkYours Seller</span></div>';
-        $.each(qnsAnsListForSeller, function () {
-            qnsAnsListString += this || '';
-        });
-        var popUpcontent2 = '</div></div></div></div>';
-        $('body').append(popUpcontent + qnsListString + popUpcontent1 + qnsAnsListString + popUpcontent2);
-        $(".faq-popup-close").click(function () {
-            $("body").removeClass("faq-open");
-        });
-        $("#faq-popup ul li a").click(function (e) {
-            e.preventDefault();
-            elemId = $(this).attr("href");
-            $(elemId).scrollTop(0);
-            $('#faq-popup .main-content').animate({scrollTop: $(elemId).offset().top - $("#qns-box").offset().top}, 500);
-        });
-        $(".navigate-top").click(function (e) {
-            e.preventDefault();
-            $('#faq-popup .main-content').animate({scrollTop: 0}, 500);
-        });
-        var url = window.location.href;
-        if (url.search('#faq-popup') > 0) {
-            setTimeout(function () {
-                $(".faq-popup-trigger")[0].click();
-            }, 500);
-        }
-
-        $(".faq-popup-trigger").click(function(){
-            $("body").addClass("faq-open");
-        });
-        $(".faq-searchbar").on('input propertychange paste', function () {
-            var searchKey = $(this).val();
-            var matchedFaqForSeller = 0;
-            var matchedFaqForShopper = 0;
-            $.each(questionAnswerForSeller, function (index, value) {
-                if (value.qns.toLowerCase().includes(searchKey.toLowerCase()) || value.ans.toLowerCase().includes(searchKey.toLowerCase())) {
-                    $("#question-seller-" + index).show();
-                    $("#link-for-seller-question-" + index).show();
-                    matchedFaqForSeller++;
-                }
-                else {
-                    $("#question-seller-" + index).hide();
-                    $("#link-for-seller-question-" + index).hide()
-                }
-            });
-            $.each(questionAnswerForShopper, function (index, value) {
-                if (value.qns.toLowerCase().includes(searchKey.toLowerCase()) || value.ans.toLowerCase().includes(searchKey.toLowerCase())) {
-                    $("#question-shopper-" + index).show();
-                    $("#link-for-shopper-question-" + index).show();
-                    matchedFaqForShopper++;
-                }
-                else {
-                    $("#question-shopper-" + index).hide();
-                    $("#link-for-shopper-question-" + index).hide();
-                }
-            });
-            if (matchedFaqForSeller == 0) {
-                $("#faqTitleSeller").hide();
-                $("#qnsAnsHeadForSeller").hide();
-                $("#qnsHeadForSeller").hide();
-            }
-            else {
-                $("#faqTitleSeller").show();
-                $("#qnsAnsHeadForSeller").show();
-                $("#qnsHeadForSeller").show();
-            }
-            if (matchedFaqForShopper == 0) {
-                $("#faqTitleShopper").hide();
-                $("#qnsAnsHeadForShopper").hide();
-                $("#qnsHeadForShopper").hide();
-            }
-            else {
-                $("#faqTitleShopper").show();
-                $("#qnsAnsHeadForShopper").show();
-                $("#qnsHeadForShopper").show();
-            }
-            $('#faq-popup .main-content').animate({scrollTop: 0}, 500);
         });
     }
 
@@ -317,73 +110,6 @@ $(document).ready(function () {
         }
     }
 
-
-    function displayLandingPage() {
-        $(".home-fluid-thumbnail-grid").empty();
-        $(".home-fluid-thumbnail-grid").css("visibility", "visible");
-        $(".home-toolbar-button-group-button").removeClass("selected");
-        $(".home-fluid-thumbnail-grid").append("<div class='row' style='border-bottom: 1px solid #c3c3c3; width: 97%'> <h3>" + featuredCategoriesLabel + "</h3></div><hr>");
-        $.map(featuredCategories, function (value, index) {
-            $(".home-fluid-thumbnail-grid").append('<div class="home-fluid-thumbnail-grid-item featured-categories">' +
-                '<div>' +
-                '<a class=" fluid-thumbnail-grid-image-item-link" href="' + value.url + '">' +
-                '<div class="fluid-thumbnail-grid-image-image-container">' +
-                '<img alt="' + value.title + '" class=" fluid-thumbnail-grid-image-image" src="' + value.image + '">' +
-                '<div class="home-fluid-thumbnail-grid-author">' +
-                '<div class="fluid-thumbnail-grid-image-title">' + value.title + '</div>' +
-                '</div>' +
-                '</div></a>' +
-                '</div></div>')
-        });
-        
-        $(".home-fluid-thumbnail-grid").append('<div class="col-12 full-width text-center pull-left" style="margin-top: 20px; height: 45px;"> <a class="view-all-listing-link-btn" href="https://www.barkyours.com/?view=grid"> VIEW ALL LISTINGS</a></div>');
-        $(".home-fluid-thumbnail-grid").append("<div class='row' style='border-bottom: 1px solid #c3c3c3; width: 97%;overflow:hidden;'> <h3>" + spotLightLabel + "</h3></div><hr>");
-        $.map(spotLights, function (item, index) {
-            if (item.type == "seller") {
-                $(".home-fluid-thumbnail-grid").append('<div class="home-fluid-thumbnail-grid-item spotlight-grid spotlight-seller">' +
-                    ' <div> <a class=" fluid-thumbnail-grid-image-item-link" href="' + item.seller_profile_url + '">' +
-                    '<div class="fluid-thumbnail-grid-image-image-container featured-seller-grid">' +
-                    ' <img alt="' + item.seller_name + '" class=" fluid-thumbnail-grid-image-image" src="' + item.image + '">' +
-                    '</div></a> ' +
-                    '<div class="home-fluid-thumbnail-grid-author">' +
-                    '<div class="spotlight-type text-center">'+item.grid_label +'</div>' +
-                    '<div class="info-container full-width text-center" >' +
-                    '<a class="home-fluid-thumbnail-grid-author-name1 seller-profile-link" title="' + item.seller_name + '" href="' + item.seller_profile_url + '">' + item.seller_name + '</a>' +
-                    '</div></div></div></div>');
-            } else {
-                $(".home-fluid-thumbnail-grid").append('<div class="home-fluid-thumbnail-grid-item spotlight-grid">' +
-                    ' <div> <a class=" fluid-thumbnail-grid-image-item-link" href="' + item.url + '">' +
-                    '<div class="fluid-thumbnail-grid-image-image-container">' +
-                    ' <img alt="' + item.title + '" class=" fluid-thumbnail-grid-image-image" src="' + item.image + '">' +
-                    '</div></a> ' +
-                    '<div class="home-fluid-thumbnail-grid-author">' +
-                    '<div class="spotlight-type text-center">'+item.grid_label +'</div>' +
-                    '<div><div class="price-container">' +
-                    '<div class="fluid-thumbnail-grid-image-price-container">' +
-                    ' <span class="fluid-thumbnail-grid-image-price">' + item.price + '</span>' +
-                    ' </div></div>' +
-                    '<div class="info-container" >' +
-                    '<div class="fluid-thumbnail-grid-image-title">' + item.title + '</div>' +
-                    '<a class="home-fluid-thumbnail-grid-author-name" title="' + item.seller_name + '" href="' + item.seller_profile_url + '">' + item.seller_name + '</a>' +
-                    '</div></div></div></div></div>');
-                elem = $(".home-fluid-thumbnail-grid").children().last();
-                var priceTag = elem.find(".fluid-thumbnail-grid-image-price-container");
-                var priceTagSpan = priceTag.find("span");
-                var price = priceTagSpan.text().replace(/(\r\n\t|\n|\r\t)/gm, "");
-                if (price.indexOf('.') == -1) {
-                    priceTagSpan.text(price + ".00");
-                }
-                var title = elem.find(".fluid-thumbnail-grid-image-title");
-                var authorContainer = elem.find(".home-fluid-thumbnail-grid-author");
-                var availableWidth = authorContainer.width() - elem.find(".price-container").width();
-                elem.find(".info-container").css("width", (availableWidth - 3) + "px");
-                elem.addClass("customized");
-            }
-        });
-
-
-    }
-
     function removeOptionalTextFromFilters() {
         $(".custom-filter-title").each(function () {
             $(this).text($(this).text().replace(/(\r\n\t|\n|\r\t)/gm, "").replace("(Optional)", ""));
@@ -413,11 +139,6 @@ $(document).ready(function () {
         }
     }
     
-    setInterval(function () {
-        $('a[href="https://barkyours.sharetribe.com/#faq-popup"]').attr("href", "#faq-popup");
-        $(".message-mood-neutral a").attr("target", "_blank");
-        $('a[href="https://blog.barkyours.com"]').attr("target", "_blank");
-    }, 500);
 
 
 
