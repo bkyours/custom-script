2f516f5dd9694e7c7bae00a3d2cc0c15010f3cd3
bkyours <38039924+bkyours@users.noreply.github.com>
2021-04-19 21:18:54 -0400

    Update contents.js

--- contents.js
+++ contents.js
@@ -75,7 +75,7 @@ var spotLights = [
     {
         "type": "seller",
         "seller_name": "Dandy Randall Wear",
-        "image": "https://blog.barkyours.com/wp-content/uploads/2021/04/Landing-PAGE-NEW-TENNIS-280-280-.jpg",
+        "image": "https://blog.barkyours.com/wp-content/uploads/2021/04/Landing-Page-NEW-TENNIS-200-200.jpg",
         "seller_profile_url": "https://www.barkyours.com/en/listings/1384010-tennis-kiss-my-ace-t-shirt-for-dog-lovers",
         "grid_label": "FEATURED PRODUCT "
     },
