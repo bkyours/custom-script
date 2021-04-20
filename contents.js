bcdc8fa805df85da739a0d1f83f9e8181583bd12
bkyours <38039924+bkyours@users.noreply.github.com>
2021-04-19 21:28:58 -0400

    Update contents.js

--- contents.js
+++ contents.js
@@ -76,15 +76,15 @@ var spotLights = [
         "type": "seller",
         "seller_name": "Dandy Randall Wear",
         "image": "https://blog.barkyours.com/wp-content/uploads/2021/04/Landing-Page-NEW-TENNIS-200-200.jpg",
-        "seller_profile_url": "https://www.barkyours.com/en/listings/1384010-tennis-kiss-my-ace-t-shirt-for-dog-lovers",
+        "seller_profile_url": "https://www.barkyours.com/en/dandyrandall",
         "grid_label": "FEATURED PRODUCT "
     },
     {
         "type": "seller",
-        "seller_name": "BarkYours Spotlight",
-        "image": "https://blog.barkyours.com/wp-content/uploads/2020/12/BarkYours-Spotlight-GIT-box-Blog-1.jpg",
-        "seller_profile_url": "https://blog.barkyours.com/",
-        "grid_label": "FEATURED BLOG "
+        "seller_name": "Captain Wiggles Co",
+        "image": "https://blog.barkyours.com/wp-content/uploads/2021/04/landing-page-summer-bandana.jpg",
+        "seller_profile_url": "https://www.barkyours.com/en/michelev",
+        "grid_label": "FEATURED PRODUCT "
     }
 ];
 
